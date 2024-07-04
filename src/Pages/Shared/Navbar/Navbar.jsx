fd899716d0e006e63ba00e500747454c9a8ff686
Raisa-islam <raisa.isl683@gmail.com>
2024-07-04 12:43:12 +0600

    initialization

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,97 +1,53 @@
 import { NavLink } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "../../../components/AuthProvider/AuthProvider";
+import logoi from '../../../assets/P_logo.png'
 
 const Navbar = () => {
-  const {user,logoutUser,reload} = useContext(AuthContext);
-  console.log(user);
-
-  const handleLogOut = () =>{
-
-    logoutUser()
-    .then()
-    .catch()
-
-  }
+ 
   return (
-    <div className="navbar bg-base-100 lg:max-w-[85%] mx-auto">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[10] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <NavLink to={"/"}>Home</NavLink>
-            </li>
-            <li>
-              <NavLink to={"/update-profile"}>Update Profile</NavLink>
-            </li>
-
-            <li>
-              <NavLink to={"/agents"}>Agents</NavLink>
-            </li>
-
-            {
-              user && (reload || user.photoURL) ? 
-              <li onClick={handleLogOut}>
-              Logout</li>
-              :
-              <li><Link to={"/login"}>Sign In</Link></li>
-              
-            }
-          
-          </ul>
-        </div>
-        <NavLink className="font-bold lg:text-3xl text-2xl" to={"/"}>UrbanAbodes</NavLink>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-        <li>
-              <NavLink to={"/"}>Home</NavLink>
-        </li>
-        <li>
-              <NavLink to={"/update-profile"}>Update Profile</NavLink>
-        </li>
-        <li>
-              <NavLink to={"/agents"}>Agents</NavLink>
-        </li>
-        </ul>
-      </div>
-      <div className="navbar-end gap-4">
-      {
-        user && (reload || user.photoURL) ? 
-        <div className="flex items-center gap-2">
-        <div className="tooltip tooltip-left cursor-pointer avatar" data-tip={user.displayName}>
-        <div className="w-10 h-10 rounded-full">
-          <img alt="user photo" src={user.photoURL} />
-        </div>
-      </div>
-        <button onClick={handleLogOut} className="btn btn-neutral text-white text-lg hidden lg:flex">Logout</button>
-        </div>
-        :
-        <Link to={"/login"} className="btn btn-outline btn-success text-white text-lg">Sign In</Link>
-
-      }  
-      </div>
+   
+    <div className="navbar bg-[#0B1930]">
+      <div className="container mx-auto">
+      <div className="flex-1">
+      <img src={logoi} className="w-12 h-12 rounded-full border-2 p-1" alt="" />
     </div>
+    <div className="flex-none">
+      <ul className= 'hidden md:flex gap-4 text-[#CCD6F6]'>
+        <li><a href=""></a>About</li>
+        <li><a href=""></a>Skills</li>
+        <li><a href=""></a>Projects</li>
+        <li><a href=""></a>Education</li>
+        <li><a href=""></a>Contact</li>
+      </ul>
+    </div>
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h16M4 18h7" />
+        </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <li><a>Homepage</a></li>
+        <li><a>Portfolio</a></li>
+        <li><a>About</a></li>
+      </ul>
+    </div>
+      </div>
+    
+   
+  </div>     
+
   );
 };
 
