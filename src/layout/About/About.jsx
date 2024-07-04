b0a5902cf4f08837aed0866b63cfaaf0d0d2a184
Raisa-islam <raisa.isl683@gmail.com>
2024-07-04 19:02:17 +0600

    about me section addes

--- src/layout/About/About.jsx
+++ src/layout/About/About.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h2 className="text-2xl font-bold text-white">About</h2>
+            <h2 className="text-2xl font-bold text-white">About Me</h2>
+            <div className=' text-gray-200 mt-12'>
+            <p>Hello! My name is Raisa Islam, and I am a passionate and enthusiastic final-year Computer Science and Engineering student. I love creating and exploring innovative solutions in the digital world. My interest in web development and programming began during my academic journey, where I immersed myself in various projects and self-initiated endeavors to deepen my understanding of software development.</p><br></br>
+
+<p>Despite not having formal work experience yet, I have completed numerous academic and personal projects that have honed my skills in web development, software engineering, and artificial intelligence. These projects have provided me with a solid foundation in HTML, CSS, JavaScript, React, and other modern technologies.</p><br></br>
+
+<p>I am currently seeking internship or job opportunities to gain practical experience and contribute to meaningful projects. I am eager to learn from industry professionals, collaborate with talented teams, and apply my knowledge in real-world scenarios. My goal is to build accessible, inclusive, and impactful digital experiences that make a difference.</p>
+
+
+            </div>
         </div>
     );
 };
